08dc9c4eb92addc848dc729dac96719cd43ac749
RodrigoGarridoLeiva <49920847+RodrigoGarridoLeiva@users.noreply.github.com>
2019-09-24 21:37:11 -0300

    Add files via upload

---
+++ firstapp/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :posts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
+
+Rails.application.routes.draw do
+  resources :muro
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
